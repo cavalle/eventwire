9c6070a904cb4fcf41c2979ccb4f14cb9034922d
Guillermo Álvarez <guillermo@cientifico.net>
2011-09-17 14:41:47 +0200

    Remove trailing spaces

--- lib/eventwire/drivers/in_process.rb
+++ lib/eventwire/drivers/in_process.rb
@@ -2,21 +2,21 @@ class Eventwire::Drivers::InProcess
   def initialize
     @handlers = {}
   end
-  
+
   def handlers(event_name)
     @handlers[event_name] ||= []
   end
-  
+
   def publish(event_name, event_data = nil)
     handlers(event_name).each do |handler|
       handler.call event_data
     end
   end
-  
+
   def subscribe(event_name, handler_id, &handler)
     handlers(event_name) << handler
   end
-  
+
   def start; end
   def stop; end
   def purge; end
--- spec/unit/drivers/drivers_helper.rb
+++ spec/unit/drivers/drivers_helper.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'timeout'
 
+
 shared_examples_for 'a driver' do
   
   def start_worker
@@ -21,35 +22,35 @@ shared_examples_for 'a driver' do
   it 'should publish to 1 event handler' do
     executed = false
     subject.subscribe(:this_event, :this_subscriber) { executed = true }
-    
+
     start_worker
     subject.publish :this_event
-    
+
     eventually {
       executed.should be_true
     }
   end
-  
+
   it 'should publish to more than 1 event handler for the event' do
     executed = [false, false]
     subject.subscribe(:this_event, :queue2) { executed[0] = true }
     subject.subscribe(:this_event, :queue1) { executed[1] = true }
-    
+
     start_worker
     subject.publish :this_event
-    
+
     eventually {
       executed.should == [true, true]
     }
   end
-  
+
   it 'should not publish to handlers for other events' do
     executed = false
     subject.subscribe(:other_event, :this_subscriber) { executed = true }
-    
+
     start_worker
     subject.publish :this_event
-    
+
     eventually {
       executed.should be_false
     }
@@ -66,7 +67,7 @@ shared_examples_for 'a driver' do
       event_data.should == { 'key1' => 'value1', 'key2' => 2 }
     }
   end
-  
+
 end
 
 shared_examples_for 'a driver with multiprocess support' do
@@ -77,7 +78,7 @@ shared_examples_for 'a driver with multiprocess support' do
     @shoutings = 0
     @ppid = Process.pid
   end
-  
+
   def process(&block)
     pid = fork do
       trap('INT') { subject.stop; subject.purge }
@@ -86,50 +87,50 @@ shared_examples_for 'a driver with multiprocess support' do
     @children << pid
     pid
   end
-  
+
   after do
     @children.each {|p| kill_and_wait(p) rescue nil }
   end
-  
+
   def kill_and_wait(pid)
     Process.kill('INT', pid)
     Process.wait(pid)
   end
-  
+
   def shout!
     Process.kill 'USR1', @ppid
   end
-  
+
   example 'one subscriber' do
-    process do   
+    process do
       subject.subscribe(:this_event, :this_subscriber) { shout! }
       subject.start
     end
-    
+
     process do
       subject.publish :this_event
     end
-    
+
     eventually do
       @shoutings.should == 1
     end
   end
-  
+
   example 'several subscribers' do
     process do
       subject.subscribe(:this_event, :first_subscriber) { shout! }
       subject.start
     end
-    
+
     process do
       subject.subscribe(:this_event, :other_subscriber) { shout! }
       subject.start
     end
-    
+
     process do
       subject.publish :this_event
     end
-    
+
     eventually do
       @shoutings.should == 2
     end
