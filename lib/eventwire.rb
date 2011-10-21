49b909a7c8806774c244cc7e1da09d46cfedb40c
Luismi Cavalle <luismi@lmcavalle.com>
2011-10-21 19:30:48 +0200

    Basic logger support. Closes #3

--- lib/eventwire.rb
+++ lib/eventwire.rb
@@ -7,12 +7,15 @@ module Eventwire
   
   def self.publish(event_name, event_data = nil)
     driver.publish event_name, event_data
+    logger.info "Event published `#{event_name}` with data `#{event_data.inspect}`"
   end
   
   def self.subscribe(event_name, handler_id, &handler)
     driver.subscribe event_name, handler_id do |data|
       begin
+        logger.info "Starting to process `#{event_name}` with handler `#{handler_id}` and data `#{data.inspect}`"
         handler.call build_event(data) 
+        logger.info "End processing `task_completed`"
       rescue Exception => ex
         @error_handler.call(ex) if @error_handler
       end
@@ -30,6 +33,14 @@ module Eventwire
   def self.driver=(driver)
     klass = Drivers.const_get(driver.to_sym) if driver.respond_to?(:to_sym)
     @driver = klass ? klass.new : driver
+  end
+  
+  def self.logger=(logger)
+    @logger = logger
+  end
+  
+  def self.logger
+    @logger ||= Logger.new(nil)
   end
   
   def self.start_worker
--- spec/unit/subscriber_spec.rb
+++ spec/unit/subscriber_spec.rb
@@ -4,6 +4,8 @@ describe Eventwire::Subscriber do
   
   describe '#on' do
     
+    before { Eventwire.on_error {|ex| raise ex } }
+    
     subject { class_including Eventwire::Subscriber }
 
     it 'should subscribe to the event using the current driver' do
@@ -14,11 +16,11 @@ describe Eventwire::Subscriber do
     
     it 'should subscribe with a handler that builds an event object' do
       Eventwire.driver.should_receive(:subscribe) do |event_name, _, &handler|
-        handler.call(:task_name => 'Cleaning').should == 'Cleaning'
-        handler.call('task_name' => 'Cleaning').should == 'Cleaning'
+        handler.call(:task_name => 'Cleaning')
+        handler.call('task_name' => 'Cleaning')
       end
       
-      subject.on(:task_completed) { |data| data.task_name }
+      subject.on(:task_completed) { |data| data.task_name.should == 'Cleaning' }
     end
     
     it 'should subscribe to the event with incremental handler ids based on the class name' do
@@ -38,6 +40,8 @@ describe Eventwire::Subscriber do
     describe 'Error handling' do
           
       it 'should subscribe with a handler that is fault tolerant' do
+        Eventwire.on_error { }
+        
         Eventwire.driver.should_receive :subscribe do |event_name, _, &handler| 
           handler.call
         end
@@ -63,6 +67,28 @@ describe Eventwire::Subscriber do
         error.message.should == 'error!'
       end
     
+    end
+    
+    describe 'Logging' do
+      
+      it 'should subscribe with a handler that logs if logger is present' do
+        ThisSubscriber = subject
+        io = StringIO.new
+        Eventwire.logger = Logger.new(io)
+        
+        Eventwire.driver.should_receive :subscribe do |event_name, _, &handler| 
+          handler.call :task_name => 'Cleaning'
+        end
+      
+        subject.on(:task_completed) { io.puts 'Hello from the handler' }
+        
+        io.string.should == <<-OUTPUT
+Starting to process `task_completed` with handler `ThisSubscriber::1` and data `{:task_name=>"Cleaning"}`
+Hello from the handler
+End processing `task_completed`
+OUTPUT
+      end
+      
     end
     
   end
