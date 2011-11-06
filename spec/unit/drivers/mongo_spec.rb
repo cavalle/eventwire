31f500e395e29d52665e05079c8385191a2f1603
Luismi Cavalle <luismi@lmcavalle.com>
2011-11-06 19:59:53 +0100

    Move mongo_spec to its proper location

--- spec/unit/drivers/mongo_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'unit/drivers/drivers_helper'
-
-describe Eventwire::Drivers::Mongo do
-  it_should_behave_like 'a driver with single-process support'
-  it_should_behave_like 'a driver with multi-process support'
-end
