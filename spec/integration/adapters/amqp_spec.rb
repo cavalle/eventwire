64f5978639b016e7c43fdcac1c58e103aa3c2bdd
Luismi Cavalle <luismi@lmcavalle.com>
2012-01-14 21:12:29 +0100

    Fix spelling

--- spec/integration/adapters/amqp_spec.rb
+++ spec/integration/adapters/amqp_spec.rb
@@ -1,6 +1,6 @@
 require 'integration/adapters/adapters_helper'
 
 describe_adapter Eventwire::Adapters::AMQP do
-  it_should_behave_like 'a adapter with single-process support'
-  it_should_behave_like 'a adapter with multi-process support'
+  it_should_behave_like 'an adapter with single-process support'
+  it_should_behave_like 'an adapter with multi-process support'
 end
